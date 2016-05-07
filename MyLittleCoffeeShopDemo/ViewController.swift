5cd661ba8b19f7caca2b3991a27168ef2c6a7069
DAMEON BRYANT <dameon@dameonbryant.com>
2016-05-07 12:41:08 -0400

    Initial Commit

---
+++ MyLittleCoffeeShopDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyLittleCoffeeShopDemo
+//
+//  Created by Dameon D Bryant on 5/7/16.
+//  Copyright © 2016 Dameon D Bryant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
